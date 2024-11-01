70809aaf2c3fd6b33ca0f5251ebbbda68e041cdf
Zehra Akbulut <zehrakbulut23@gmail.com>
2024-11-01 17:41:37 +0300

    random sayıyı tahmin etme

--- csharp-algorithm-examples/Program.cs
+++ csharp-algorithm-examples/Program.cs
@@ -12,11 +12,7 @@ namespace csharp_algorithm_examples
         {
 
 
-          //Soru: bir tahmin oyunu oluşturmanı istiyorum.Öncelikle random bir sayı üretilsin
-          //ve bu bir değişkende tutulsun. Kullanıcı 1 - 100(1 ve 100 dahil) arası tahmin edecek
-          //ve toplamda 5 deneme hakkı olacak.Eğer deneme hakkı biterse oyun sonlanacak. 
-          //Her tahmin sonucu sayının küçük büyük olduğu bilgisi kullanıcıya verilsin. Kullanıcı
-          //doğru sayıyı tahmin ederse oyun sonlansın.
+          
 
             Random rastgele = new Random();
             int sayi = rastgele.Next(1, 11);
