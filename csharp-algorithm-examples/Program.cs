1e82bd98e37d29f283d35e761b43f8ae02d81b5c
Zehra Akbulut <zehrakbulut23@gmail.com>
2024-11-01 17:45:32 +0300

    random sayıyı sonsuz döngüde bulma

--- csharp-algorithm-examples/Program.cs
+++ csharp-algorithm-examples/Program.cs
@@ -10,6 +10,42 @@ namespace csharp_algorithm_examples
     {
         static void Main(string[] args)
         {
+            Random rastgele = new Random();
+            int sayi = rastgele.Next(1, 11);
+
+            int i = 0;
+            int tahmin = 0;
+            Console.WriteLine("sayı tahmin oyunu başlasın");
+            Console.WriteLine(sayi);
+
+            while (tahmin != sayi)
+            {
+                i++;
+                Console.Write("{0} .tahmin: ", i);
+                tahmin = Convert.ToInt32(Console.ReadLine()); 
+                if (tahmin == sayi)
+                {
+                    Console.WriteLine("kazandınız");
+                    return;
+                    //Environment.Exit(0);  //buda olabilir
+                }
+                else if (tahmin < sayi)
+
+                {
+                    Console.Write("daha büyük.. ");
+                }
+                else
+                {
+                    Console.Write("daha küçük..");
+                }
+                if (tahmin < 0 || tahmin > 100)
+                {
+                    Console.WriteLine("girdiğiniz sayı geçersizdir..");
+                }
+            }
+
+
+            Console.ReadLine();
         }
     }
 }
