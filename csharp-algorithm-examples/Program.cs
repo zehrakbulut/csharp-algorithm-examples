d2435e852a3cf73efcb5e6612ce218cdfe395857
Zehra Akbulut <zehrakbulut23@gmail.com>
2024-11-01 17:27:56 +0300

    birinci tahmin oyunu kodu eklendi

--- csharp-algorithm-examples/Program.cs
+++ csharp-algorithm-examples/Program.cs
@@ -10,6 +10,43 @@ namespace csharp_algorithm_examples
     {
         static void Main(string[] args)
         {
+
+
+          //Soru: bir tahmin oyunu oluşturmanı istiyorum.Öncelikle random bir sayı üretilsin
+          //ve bu bir değişkende tutulsun. Kullanıcı 1 - 100(1 ve 100 dahil) arası tahmin edecek
+          //ve toplamda 5 deneme hakkı olacak.Eğer deneme hakkı biterse oyun sonlanacak. 
+          //Her tahmin sonucu sayının küçük büyük olduğu bilgisi kullanıcıya verilsin. Kullanıcı
+          //doğru sayıyı tahmin ederse oyun sonlansın.
+
+            Random rastgele = new Random();
+            int sayi = rastgele.Next(1, 11);
+
+            Console.WriteLine("aklımdaki sayı sence ne? " + sayi);
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("tahmininizi giriniz: ");
+                int tahmin = Convert.ToInt32(Console.ReadLine());
+
+                if (tahmin == sayi)
+                {
+                    Console.WriteLine("kazandınız");
+                    break;
+                }
+                else if (tahmin > sayi)
+                {
+                    Console.WriteLine("daha küçük");
+                }
+                else
+                {
+                    Console.WriteLine("daha büyük");
+                }
+
+                if (tahmin < 0 || tahmin > 101)
+                {
+                    Console.WriteLine("geçersiz tahmin");
+                }
+            }
         }
     }
 }
