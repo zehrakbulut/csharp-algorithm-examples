a17efade8d9df9bf17613f3617d43748fa00abd0
Zehra Akbulut <zehrakbulut23@gmail.com>
2024-11-01 21:48:23 +0300

    girilen iki sayı arasındaki sayıların toplamı ve ortalamasi

--- csharp-algorithm-examples/Program.cs
+++ csharp-algorithm-examples/Program.cs
@@ -10,6 +10,30 @@ namespace csharp_algorithm_examples
     {
         static void Main(string[] args)
         {
+            int toplam = 0; int ortalama = 0;
+            int sayi1, sayi2;
+
+            Console.WriteLine("sayı giriniz: ");
+            sayi1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("2.sayı: ");
+            sayi2 = Convert.ToInt32(Console.ReadLine());
+
+
+            if (sayi1 > sayi2)
+            {
+                int sayi = sayi1;
+                sayi1 = sayi2;
+                sayi2 = sayi;
+            }
+
+            for (int i = sayi1; i <= sayi2; i++)
+            {
+                toplam += i;
+            }
+
+            Console.WriteLine("toplam= " + toplam);
+            ortalama = toplam / (sayi2 - sayi1);
+            Console.WriteLine("ortalama= " + ortalama);
         }
     }
 }
