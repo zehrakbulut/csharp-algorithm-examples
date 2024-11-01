6dde54675bf7e7dbc380723481e69bd4fb1a7088
Zehra Akbulut <zehrakbulut23@gmail.com>
2024-11-01 21:20:24 +0300

    sifir girilince bitir

--- csharp-algorithm-examples/Program.cs
+++ csharp-algorithm-examples/Program.cs
@@ -10,6 +10,30 @@ namespace csharp_algorithm_examples
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Sayı giriniz:");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            int toplam = 0;
+            int sayac = 0;
+
+            while (sayi != 0)
+            {
+                toplam += sayi;
+                sayac++;
+
+                Console.WriteLine("Sayı giriniz: ");
+                sayi = Convert.ToInt32(Console.ReadLine());
+            }
+
+            if (sayac > 0)
+            {
+                Console.WriteLine("Girilen sayıların toplamı={0} Ortalaması={1}", toplam, toplam / sayac);
+            }
+            else
+            {
+                Console.WriteLine("sayı girilmedi");
+            }
+
+            Console.ReadKey();
         }
     }
 }
