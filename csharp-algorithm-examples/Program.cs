c7254185cb8d52495d6fec82c56dc0cf0bcb7bf5
Zehra Akbulut <zehrakbulut23@gmail.com>
2024-11-18 20:00:02 +0300

    atm islem

--- csharp-algorithm-examples/Program.cs
+++ csharp-algorithm-examples/Program.cs
@@ -10,29 +10,61 @@ namespace csharp_algorithm_examples
     {
         static void Main(string[] args)
         {
-            int sayi = 0;
-            int max = int.MinValue;
-            int min = int.MaxValue;
+            //1
+            //kullanıcıya sorucaz nasıl bir işlem yapmak istediğini
+            //1 - bakiye görüntüleme
+            //2 - para çekme
+            //3 - para yatırma
+            //q ya basarsa -çıkış yapma
 
-            for (int i = 0; i < 10; i++)
+
+
+            int bakiye = 1000;
+
+            Console.WriteLine("yapılıcak işlemi seçiniz: ");
+            Console.WriteLine("1-bakiye görüntüleme");
+            Console.WriteLine("2-para çekme");
+            Console.WriteLine("3-para yatırma");
+            Console.WriteLine("q ya basarsa çıkış yapma");
+
+            string islem = Console.ReadLine();
+
+            switch (islem)
             {
-                Console.WriteLine("sayı giriniz: ");
-                sayi = Convert.ToInt32(Console.ReadLine());
+                case "1":
+                    Console.WriteLine("bakiye: " + bakiye);
+                    break;
 
-                if (sayi > max)
-                {
-                    max = sayi;
-                }
-                if (sayi < min)
-                {
-                    min = sayi;
-                }
+                case "2":
+                    Console.Write("çekmek istediğiniz tutar: ");
+                    int cekilicekTutar = int.Parse(Console.ReadLine());
+                    if (cekilicekTutar < bakiye)
+                    {
+                        Console.WriteLine("kalan bakiye: " + (bakiye - cekilicekTutar));
+                    }
+                    else
+                    {
+                        Console.WriteLine("yetersiz bakiye!!!");
+                    }
+                    break;
+
+                case "3":
+                    Console.Write("yatırmak istediğiniz tutar: ");
+                    int yatirilicakTutar = int.Parse(Console.ReadLine());
+                    Console.WriteLine("yeni bakiye: " + (bakiye + yatirilicakTutar));
+                    break;
+
+                case "q":
+                    Console.WriteLine("çıkış yapılıyor");
+                    break;
+
+                default:
+                    Console.WriteLine("geçersiz tuşlama...");
+                    break;
+
+
             }
-            Console.WriteLine("en küçük sayı: " + min);
-            Console.WriteLine("en büyük sayı: " + max);
 
-
-            Console.ReadLine();
         }
     }
 }
