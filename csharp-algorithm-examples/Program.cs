c145e5dee98fb741038ecd66a4b35ba9d0113d16
Zehra Akbulut <zehrakbulut23@gmail.com>
2024-11-22 17:04:22 +0300

    girilen sayi asalMi

--- csharp-algorithm-examples/Program.cs
+++ csharp-algorithm-examples/Program.cs
@@ -13,26 +13,7 @@ namespace csharp_algorithm_examples
         static void Main(string[] args)
         {
 
-            //1-100 asal sayıları yazdırma
-
-            for (int i =2;  i<100; i++)
-            {
-                bool asalMi = true;
-
-                for (int j =2; j<i; j++)
-                {
-                    if (i % j == 0)
-                    {
-                        asalMi = false;
-                        break;
-                    }
-                }
-
-                if (asalMi)
-                {
-                    Console.WriteLine(i);
-                }
-            }           
+                    
         }
     }
 }
