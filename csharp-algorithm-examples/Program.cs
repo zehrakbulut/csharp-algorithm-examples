5bd0cb3b15d92461ffec5667659955ba3fb3ae3c
Zehra Akbulut <zehrakbulut23@gmail.com>
2024-12-01 13:36:03 +0300

    consonant-printing

--- csharp-algorithm-examples/Program.cs
+++ csharp-algorithm-examples/Program.cs
@@ -13,40 +13,19 @@ namespace csharp_algorithm_examples
     {
         static void Main(string[] args)
         {
-            // decimal to binary...
+            // sessiz harfleri yazdırma...
 
-            Console.Write("lütfen sayı giriniz: ");
-            string input = Console.ReadLine();
-
-            List<int> numbers = new List<int>();  //sayıları liste
-
-            if(int.TryParse(input, out int sayi))
+                Console.WriteLine("lütfen bir kelime giriniz: ");
+            string kelime = Console.ReadLine();
+            char[] sesliHarfler = { 'a', 'e', 'ı', 'i', 'u', 'ü', 'o', 'ö' };
+            
+            foreach (char harf in sesliHarfler)
             {
-                int bolum=1;
-                int kalan;
-
-                while (bolum != 0)
-                {
-                    bolum = sayi / 2;
-                    kalan = sayi % 2;
-
-                    sayi = bolum;
-
-                    numbers.Add(kalan);
-                }
+                kelime = kelime.ToLower().Replace(harf.ToString(), "");
             }
+            Console.WriteLine(kelime);
 
-            else
-            {
-                Console.Write("geçersiz giriş..");
-            }
-
-            for (int i = numbers.Count() - 1; i >= 0; i--)
-            {
-                Console.Write(numbers[i]);
-            }
-
-            Console.ReadLine();
+            Console.Read();
         }
     }
 }
