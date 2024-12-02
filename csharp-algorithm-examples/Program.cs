043a7155c797058d941795506526db6a2ca1a2ee
Zehra Akbulut <zehrakbulut23@gmail.com>
2024-12-02 14:13:07 +0300

    celcius to F an K

--- csharp-algorithm-examples/Program.cs
+++ csharp-algorithm-examples/Program.cs
@@ -10,6 +10,13 @@ namespace csharp_algorithm_examples
     {
         static void Main(string[] args)
         {
+            //santigrat'ı kelvine ve fahrenheit'e çevirme
+
+            Console.Write("hava kaç °C: ");
+            double celsius = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("{0} + (273,15) = {1}K ", celsius, (celsius + 273, 15));
+            Console.WriteLine("{0} * 1.8 + 32  = {1}°F ", celsius, (celsius * 18/10 + 32));
         }
     }
 }
