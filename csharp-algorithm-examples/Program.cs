e55451968a2b9ed14b1ace2e26e3437ac8dbdb2d
Zehra Akbulut <zehrakbulut23@gmail.com>
2024-12-04 14:08:02 +0300

    starts with hello

--- csharp-algorithm-examples/Program.cs
+++ csharp-algorithm-examples/Program.cs
@@ -10,6 +10,25 @@ namespace csharp_algorithm_examples
     {
         static void Main(string[] args)
         {
+            //Bir dizenin belirtilen bir kelime ile başlayıp başlamadığını kontrol eden bir C# programı yazın.
+
+            string kelime = "Merhaba";
+
+            Console.WriteLine("Lütfen bir cümle yazınız: ");
+            string cumle = Console.ReadLine();
+
+            bool varMi = false;
+
+            //if (cumle.Length < 8 && cumle.Equals("Merhaba") || cumle.StartsWith("Merhaba") && cumle[7] == ' ')
+            if (cumle.Contains(kelime))
+            {
+                varMi = true;
+                Console.WriteLine(varMi);
+            }
+            else
+            {
+                Console.WriteLine(varMi);
+            }
         }
     }
 }
