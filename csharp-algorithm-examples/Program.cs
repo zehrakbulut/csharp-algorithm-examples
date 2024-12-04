8622e62288121bb1179d10a786e4ccdfe9331ddb
Zehra Akbulut <zehrakbulut23@gmail.com>
2024-12-04 22:38:41 +0300

    sum array integers

--- csharp-algorithm-examples/Program.cs
+++ csharp-algorithm-examples/Program.cs
@@ -10,6 +10,15 @@ namespace csharp_algorithm_examples
     {
         static void Main(string[] args)
         {
+            //Bir tamsayı dizisinin tüm elemanlarının toplamını hesaplayan bir C# programı yazın.
+
+            int[] array1 = { 1, 2, 2, 3, 3, 4, 5, 6, 5, 7, 7, 7, 8, 8, 1 };
+            int sum = 0;
+            for(int i = 0; i<array1.Length; i++)
+            {
+                sum += array1[i];
+            }
+            Console.WriteLine(sum);
         }
     }
 }
