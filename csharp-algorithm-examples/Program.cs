2e1cde99c2e5093b5bab1ae74b0efd0760f2c8f9
Zehra Akbulut <zehrakbulut23@gmail.com>
2024-12-04 11:05:38 +0300

    positive number multiple of 3 or 7

--- csharp-algorithm-examples/Program.cs
+++ csharp-algorithm-examples/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,26 @@ namespace csharp_algorithm_examples
     {
         static void Main(string[] args)
         {
+            Console.Write("lütfen pozitif bir tamsayı giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            bool katiMi = false;
+            if(sayi > 0)
+            {
+                if(sayi % 3 == 0 || sayi % 7 == 0)
+                {
+                    katiMi = true;
+                    Console.WriteLine(katiMi);
+                }
+                else
+                {
+                    Console.WriteLine(katiMi);
+                }
+            }
+            else
+            {
+                Console.Write("lütfen 0'dan büyük bir tamsayı giriniz..");
+            }
         }
     }
 }
